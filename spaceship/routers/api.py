12f277063dec2385332520607883ba73ce62d16b
Nazg0r <gorchinshiy83@gmail.com>
2024-04-01 13:57:11 +0300

    Add new endpoint

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,21 @@
 from fastapi import APIRouter
-
+from numpy import random
 router = APIRouter()
 
 
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+@router.get('/multMatrix')
+def get_matrix_multiplication():
+    matrix_a = random.randint(0, 100, size=(10, 10))
+    matrix_b = random.randint(0, 100, size=(10, 10))
+    product = matrix_a @ matrix_b
+    
+    return {
+        "matrix_a": str(matrix_a.tolist()),
+        "matrix_b": str(matrix_b.tolist()),
+        "product": str(product.tolist())
+    }
